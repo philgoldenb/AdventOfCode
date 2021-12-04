c94fe8bc88da667d41f48fd1ffaaaf025c34db3c
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-03 21:59:44 -0500

    Break input parsing out into auxiiliary function

--- 2021/day2/day2_2.py
+++ 2021/day2/day2_2.py
@@ -1,11 +1,14 @@
 INPUT_FILE = "/Users/phil/repos/AdventOfCode/2021/day2/input.txt"
 
+def parse_instruction(instruction):
+    direction, value = instruction.split()
+    return direction, int(value)
+
 horizontal, depth, aim = 0, 0, 0
 
 with open(INPUT_FILE, "r") as file:
     for instruction in file:
-        direction, value = instruction.split() 
-        value = int(value)
+        direction, value = parse_instruction(instruction)
 
         if direction == "forward":
             horizontal += value
