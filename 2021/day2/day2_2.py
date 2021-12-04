d90c8699f17d099dce301a40c14fba2b590cc0ae
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-03 22:10:29 -0500

    Enumerate constants

--- 2021/day2/day2_2.py
+++ 2021/day2/day2_2.py
@@ -1,7 +1,15 @@
+from enum import Enum
+
 INPUT_FILE = "/Users/phil/repos/AdventOfCode/2021/day2/input.txt"
+
+class Direction(Enum):
+    FORWARD = 'forward'
+    DOWN = 'down'
+    UP = 'up'
 
 def parse_instruction(instruction):
     direction, value = instruction.split()
+    direction = Direction(direction)
     return direction, int(value)
 
 horizontal, depth, aim = 0, 0, 0
@@ -10,12 +18,12 @@ with open(INPUT_FILE, "r") as file:
     for instruction in file:
         direction, value = parse_instruction(instruction)
 
-        if direction == "forward":
+        if direction == Direction.FORWARD:
             horizontal += value
             depth += aim * value
-        elif direction == "down":
+        elif direction == Direction.DOWN:
             aim += value
-        elif direction == "up":
+        elif direction == Direction.UP:
             aim -= value
         else:
             raise ValueError("Invalid Direction")
