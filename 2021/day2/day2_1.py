b72890bee9d39293a0c01ca4e1404f8791b473a8
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-03 21:04:31 -0500

    Refactor

--- 2021/day2/day2_1.py
+++ 2021/day2/day2_1.py
@@ -1,19 +1,24 @@
 def position(instructions):
-    depth = 0
-    horizontal = 0
-    for instr in instructions:
-        if instr[0] == "forward":
-            horizontal += int(instr[1])
-        elif instr[0] == "down":
-            depth += int(instr[1])
-        elif instr[0] == "up":
-            depth -= int(instr[1])
+
+    horizontal, depth = 0, 0
+    for instruction in instructions:
+        direction, value = instruction.split() 
+        value = int(value)
+
+        if direction == "forward":
+            horizontal += value
+        elif direction == "down":
+            depth += value
+        elif direction == "up":
+            depth -= value
+        else:
+            raise ValueError("Invalid Direction")
 
     return horizontal * depth
 
 input_file = "/Users/phil/repos/AdventOfCode/2021/day2/input.txt"
 with open(input_file,"r") as file:
-    instructions = [l.split() for l in file]
+    instructions = file.readlines()
 
 
 print(position(instructions))
