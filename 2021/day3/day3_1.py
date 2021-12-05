0fecd4ddd0cd59fbdf923160e0a64067816a2c4d
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-04 20:09:34 -0500

    Refactor day3 pt 1

--- 2021/day3/day3_1.py
+++ 2021/day3/day3_1.py
@@ -1,25 +1,21 @@
-
 INPUT_FILE = 'day3/input.txt'
 
 with open(INPUT_FILE, 'r') as file:
-    #contents = file.readlines()
     diagnostics = [line.rstrip() for line in file]
 
 gamma, epsilon = "", ""
 for i in range(len(diagnostics[0])):
-    count_0s, count_1s = 0, 0
 
+    counts = {"0": 0, "1": 0}
     for reading in diagnostics:
-        if reading[i] == "0":
-            count_0s += 1
-        elif reading[i] == "1":
-            count_1s += 1
-    
-    most_common_bit = "1" if count_1s >= count_0s else "0"
-    least_common_bit = "0" if count_1s >= count_0s else "1"
+        counts[reading[i]] += 1
 
-    gamma += most_common_bit
-    epsilon += least_common_bit
+    if counts["0"] > counts["1"]:
+        gamma += "0"
+        epsilon += "1"
+    else:
+        gamma += "1"
+        epsilon += "0"
 
 gamma_dec = int(gamma, 2)
 epsilon_dec = int(epsilon, 2)
