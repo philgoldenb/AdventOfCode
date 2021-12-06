bed73c158300dfde217b6db47bc1f43e38b881bd
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-05 20:39:00 -0500

    Use bitwise ops to build gamma & epsilon as integers

--- 2021/day3/day3_1.py
+++ 2021/day3/day3_1.py
@@ -1,4 +1,4 @@
-from collections import Counter, defaultdict
+from collections import Counter
 
 INPUT_FILE = 'day3/input.txt'
 
@@ -7,17 +7,12 @@ with open(INPUT_FILE, 'r') as file:
     for line in file:
         counts += Counter(enumerate(line.rstrip()))
 
-line_len = len(counts) // 2
-gamma = ["0" for _ in range(line_len)]
-epsilon = gamma[::]
-for col in range(line_len):
-    count_0s, count_1s = counts[(col, "0")], counts[col,"1"]
-    if count_0s > count_1s:
-        epsilon[col] = "1"
+gamma, epsilon = 0, 0
+total_bits = len(counts) // 2
+for col in range(total_bits):
+    if counts[(col, "0")] > counts[col, "1"]:
+        epsilon |= 1 << (total_bits - col - 1)
     else:
-        gamma[col] = "1"
+        gamma |= 1 << (total_bits - col - 1)
 
-gamma_dec = int("".join(gamma), 2)
-epsilon_dec = int("".join(epsilon), 2)
-
-print(gamma_dec * epsilon_dec)
+print(gamma * epsilon)
