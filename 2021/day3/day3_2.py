1acadd6f64aad693d361118457ea65f05a11a346
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-06 21:22:41 -0500

    Initial embarassing, but working solution to day3 pt2

---
+++ 2021/day3/day3_2.py
@@ -0,0 +1,65 @@
+from collections import Counter
+
+INPUT_FILE = 'day3/input.txt'
+
+with open(INPUT_FILE,'r') as file:
+    oxy_contents = [line.strip() for line in file]
+co2_contents = [line for line in oxy_contents]
+
+def filt_ones(rows):
+    for line in rows:
+        if line[0] == "1":
+            return True
+        else:
+            return False
+
+def filt_zeros(rows):
+    for line in rows:
+        if line[0] == '0':
+            return True
+        else:
+            return False
+
+row_len = len(oxy_contents[0])
+
+for col in range(row_len):
+    oxy_counts = Counter()
+
+    for line in oxy_contents:
+        oxy_counts[line[col]] += 1
+
+    zeros, ones = oxy_counts["0"], oxy_counts["1"]
+
+    if ones >= zeros:
+        oxy_contents = [row for row in oxy_contents if row[col] == "1"]
+    else:
+        oxy_contents = [row for row in oxy_contents if row[col] == "0"]
+
+    if len(oxy_contents) == 1:
+        break
+
+
+for col in range(row_len):
+    
+    co2_counts = Counter()
+    for line in co2_contents:
+        co2_counts[line[col]] += 1
+    
+    zeros, ones = co2_counts["0"], co2_counts["1"]
+
+    if zeros <= ones:
+        co2_contents = [row for row in co2_contents if row[col] == "0"]
+    else:
+        co2_contents = [row for row in co2_contents if row[col] == "1"]
+    
+    if len(co2_contents) == 1:
+        break
+
+
+oxy_gen = oxy_contents[0]
+co2_gen = co2_contents[0]
+
+print(f"oxy_gen is {oxy_gen}")
+print(f"co2_gen is {co2_gen}")
+
+print(f"Life Support Rating = {int(oxy_gen, 2) * int(co2_gen, 2)}")
