3cfc268e295e0a87d5cda4b36e351c8878ab2df5
Phil Goldenberg <philgoldenb@gmail.com>
2021-12-07 08:50:40 -0500

    refactor: Put similar functionality into function, pass in lambda predicate

--- 2021/day3/day3_2.py
+++ 2021/day3/day3_2.py
@@ -5,28 +5,21 @@ INPUT_FILE = 'day3/input.txt'
 with open(INPUT_FILE,'r') as file:
     contents = file.readlines()
 
-row_len = len(contents[0])
+def compute_rating(contents, filter_criteria):
+    row_len = len(contents[0])
 
-oxy_contents = contents
-for col in range(row_len):
-    counts = Counter(line[col] for line in oxy_contents)
-    most_common_val = "1" if counts["1"] >= counts["0"] else "0"
-    oxy_contents = [row for row in oxy_contents if row[col] == most_common_val]
-    if len(oxy_contents) == 1:
-        break
+    for col in range(row_len):
+        counts = Counter(line[col] for line in contents)
+        filter_val = "1" if filter_criteria(counts["1"], counts["0"]) else "0"
+        contents = [row for row in contents if row[col] == filter_val]
 
+        if len(contents) == 1:
+            break
+    
+    return contents[0]
 
-co2_contents = contents
-for col in range(row_len):
-    counts = Counter(line[col] for line in co2_contents)
-    least_common_val = "1" if counts["1"] < counts["0"] else "0"
-    co2_contents = [row for row in co2_contents if row[col] == least_common_val]
-    if len(co2_contents) == 1:
-        break
-
-
-oxy_gen = oxy_contents[0]
-co2_gen = co2_contents[0]
+oxy_gen = compute_rating(contents, lambda ones, zeros: ones >= zeros)
+co2_gen = compute_rating(contents, lambda ones, zeros: ones < zeros)
 
 print(f"oxy_gen is {oxy_gen}")
 print(f"co2_gen is {co2_gen}")
